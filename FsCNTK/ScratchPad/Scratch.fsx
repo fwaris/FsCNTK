80c009ed05f1c741f8f8280397e328b400fa6799
Faisal Waris <faisalwaris@live.com>
2019-04-17 21:11:25 -0400

    remove extra files

--- FsCNTK/ScratchPad/Scratch.fsx
+++ FsCNTK/ScratchPad/Scratch.fsx
@@ -1,17 +1,8 @@
 ﻿#load "..\Scripts\SetEnv.fsx"
 open FsCNTK
-open FsCNTK.FsBase
-open Layers_Dense
-open Layers_Sequence
-open Models_Attention
-open CNTK
-open System.IO
-open System
-open Blocks
 
-
-let flag = V.toValue [10.; -1.; -0.; 0.3; 100.]
-let value_if_true = V.toValue [1.; 10.; 100.; 1000.; 10000.]
-let value_if_false = V.toValue [ 2.; 20.; 200.; 2000.; 20000.]
-let v = O.element_select(Node.Const flag.Data, Node.Const value_if_true.Data, Node.Const value_if_false.Data)
+let flag = [10.; -1.; -0.; 0.3; 100.]
+let value_if_true =  [1.; 10.; 100.; 1000.; 10000.]
+let value_if_false = [ 2.; 20.; 200.; 2000.; 20000.]
+let v = O.element_select(Node.Const flag, Node.Const value_if_true, Node.Const value_if_false)
 let v2 =  v |> E.eval1(idict[])
