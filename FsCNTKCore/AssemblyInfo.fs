8271c9ea1811f3fd1d798364451caf0ae713df2e
Faisal Waris <faisalwaris@live.com>
2020-04-26 07:08:31 -0400

    net5.0 version

--- FsCNTKCore/AssemblyInfo.fs
+++ FsCNTKCore/AssemblyInfo.fs
@@ -7,12 +7,12 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("FsCNTK")>]
+[<assembly: AssemblyTitle("FsCNTKCore")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("Faisal Waris")>]
-[<assembly: AssemblyCopyright("Copyright ©  2018")>]
+[<assembly: AssemblyCopyright("Copyright ©  2020")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
@@ -22,7 +22,7 @@ open System.Runtime.InteropServices
 [<assembly: ComVisible(false)>]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[<assembly: Guid("69928b58-4265-48a0-9817-2a5772d7c4dc")>]
+[<assembly: Guid("69928b58-4265-48a0-9817-2a5772d7c4dd")>]
 
 // Version information for an assembly consists of the following four values:
 // 
@@ -34,8 +34,8 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("1.0.0.1")>]
+[<assembly: AssemblyFileVersion("1.0.0.1")>]
 
 do
     ()
